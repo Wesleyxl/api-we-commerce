5ca2aaeab95891ceb8ab2ff9e25f666d5408fe07
wesleyxl <wesley.alvesxll@gmail.com>
2022-08-11 17:33:06 -0300

    create category controller

---
+++ src/app/controller/CategoryController.js
@@ -0,0 +1,62 @@
+const Category = require("@model/Category");
+
+module.exports = {
+  async index(req, res) {
+    const categories = await Category.findAll();
+
+    if (categories.length <= 0) {
+      return res.status(400).json({ error: "No categories found" });
+    }
+
+    return res.json(categories);
+  },
+
+  // show existing category
+  async show(req, res) {
+    const { id_category } = req.params;
+
+    const category = await Category.findByPk(id_category);
+
+    if (!category) {
+      return res.status(400).json({ error: "Category not found" });
+    }
+
+    return res.json(category);
+  },
+
+  // create new category
+  async store(req, res) {
+    const { name } = req.body;
+
+    if (!name || name === "") {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const category = await Category.create({ name });
+
+    return res.json(category);
+  },
+
+  // update existing category
+  async update(req, res) {
+    const { id_category } = req.params;
+    const { name } = req.body;
+
+    if (!name || name === "") {
+      return res.status(400).json({ error: "Name is required" });
+    }
+
+    const category = await Category.update(
+      { name },
+      { where: { id: id_category } }
+    );
+
+    if (category) {
+      return res.json(category);
+    }
+
+    return res.status(400).json({ error: "Something went wrong" });
+  },
+
+  // delete existing category
+};
--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ const router = express.Router();
 // controllers
 const AuthController = require("@controller/AuthController");
 const UserController = require("@controller/UserController");
+const CategoryController = require("@controller/CategoryController");
+const ProductController = require("@controller/ProductController");
 
 // auth routers
 router.post("/auth/sign-in", AuthController.signIn);
@@ -16,5 +18,13 @@ router.get("/auth/me", auth, AuthController.me);
 // user routes
 router.post("/user/delete/", auth, UserController.delete);
 // router.get("/user", auth, UserController.index);
+
+// category routes
+router.get("/categories", CategoryController.index);
+router.get("/category/:category_id", CategoryController.store);
+router.post("/category", CategoryController.store);
+router.put("/category/:id", CategoryController.update);
+
+// products routes
 
 module.exports = router;
